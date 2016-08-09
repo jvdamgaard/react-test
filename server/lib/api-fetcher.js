2a10680c7fa31f4a76517b6366e506e010cf282b
Jakob Viskum Damgaard <jakob.viskum.damgaard@gmail.com>
2016-08-09 15:39:12 +0200

    Cache size in megabytes + correct maxAge

--- server/lib/api-fetcher.js
+++ server/lib/api-fetcher.js
@@ -1,10 +1,13 @@
 import LRU from 'lru-cache';
 import axios from 'axios';
 
+const CHARS_PER_MEGABYTE = 1048576; // Based on http://www.unitconversion.org/data-storage/characters-to-megabytes-conversion.html
+
 const fetcher = axios.create();
 const cache = new LRU({
-  max: 10000,
-  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  max: CHARS_PER_MEGABYTE * 1000,
+  maxAge: 10 * 60 * 1000, // 10 min
+  length: (n) => JSON.stringify(n).length,
 });
 
 export default function apiFetcher(request) {
@@ -18,15 +21,12 @@ export default function apiFetcher(request) {
     .get(request)
     .then(response => {
       // Save success responses in cache
-      if (
-        response.status >= 200 &&
-        response.status < 300 &&
-        response.headers['cache-control'] &&
-        response.headers['cache-control'].indexOf('public, max-age=') === 0
-      ) {
-        const time = parseInt(response
-          .headers['cache-control']
-          .replace('public, max-age=', 10), 10);
+      if (response.status >= 200 && response.status < 300) {
+        let time;
+        const cacheControl = response.headers['cache-control'];
+        if (cacheControl && cacheControl.indexOf('public, max-age=') === 0) {
+          time = parseInt(cacheControl.replace('public, max-age=', ''), 10) * 1000;
+        }
         cache.set(request, response.data, time);
       }
       return response;
